fcf2fdd906e46f7d0661c9ba334c42d463062cb3
Andrés Londoño <andres.londono@teaminternational.com>
2019-08-19 10:14:34 -0500

    fiexd logic to choose secret friend

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -22,19 +22,38 @@ export class AppComponent implements OnInit {
     let tm = timer(1000);
     tm.subscribe(done => {
 
-      this.users.push({ userId: Guid.newGuid(), userName: 'Andrés Londoño', created: null });
-      this.users.push({ userId: Guid.newGuid(), userName: 'Alejandra Rendon', created: new Date() });
-      this.users.push({ userId: Guid.newGuid(), userName: 'Ismael Londoño', created: null });
-      this.users.push({ userId: Guid.newGuid(), userName: 'Piedad Mesa', created: new Date() });
-      this.users.push({ userId: Guid.newGuid(), userName: 'Sebastian Londoño', created: null });
 
+      let andresId = Guid.newGuid();
+      let piedadId = Guid.newGuid();
+      let alejaId = Guid.newGuid();
+      let IsmaelId = Guid.newGuid();
+      let SebastianId = Guid.newGuid();
+
+      this.users.push({ userId: andresId, userName: 'Andrés Londoño' });
+      this.users.push({ userId: Guid.newGuid(), userName: 'Jhonatan Londoño' });
+      this.users.push({ userId: Guid.newGuid(), userName: 'Jorge Londoño' });
+      this.users.push({ userId: Guid.newGuid(), userName: 'Andrea Rendón' });
+      this.users.push({ userId: Guid.newGuid(), userName: 'Rosalba Loaiza' });
+      this.users.push({ userId: Guid.newGuid(), userName: 'Zaritha Hernandez' }); 
+
+      this.users.push({ userId: alejaId, userName: 'Alejandra Rendon', created: new Date(), friendId: SebastianId });
+      this.users.push({ userId: IsmaelId, userName: 'Ismael Londoño' , created: new Date(), friendId: IsmaelId});
+      this.users.push({ userId: piedadId, userName: 'Piedad Mesa', created: new Date(), friendId: andresId });
+      this.users.push({ userId: SebastianId, userName: 'Sebastian Londoño', created: new Date(), friendId: alejaId });
 
       this.exceptions.push({
         userId: this.users[0].userId,
         userName: this.users[0].userName,
         friendId: this.users[2].userId,
         friendName: this.users[2].userName
-      })
+      });
+
+      this.exceptions.push({
+        friendId: this.users[0].userId,
+        friendName: this.users[0].userName,
+        userId: this.users[2].userId,
+        userName: this.users[2].userName
+      });
 
       this.isLoading = false;
     })
@@ -44,7 +63,7 @@ export class AppComponent implements OnInit {
     this.pagetoShow = 2;
     let tmInterval = interval(1000);
     tmInterval.subscribe(i => {
-      if (this.intervalNumber === 0) {
+      if (this.intervalNumber === 1) {
         this.pagetoShow = 3;
       } else {
         this.intervalNumber = this.intervalNumber - 1;
@@ -54,22 +73,29 @@ export class AppComponent implements OnInit {
   generateSecretFriend(userId: string) {
     debugger;
     while (this.secretFriend == null) {
-      var validUsers = this.users.filter(function (x) {
-        return x.created == null && x.friendId == null;
-      });
-      var random = Math.floor(Math.random() * validUsers.length) + 0;
-      var friend = validUsers[random];
-      //El amigo secreto debe ser diferente a el usuario seleccionado
-      if (friend.userId != userId) {
-        //El amigo secreto no debe estar como excepcion
-        var userExceptions = this.exceptions.filter(function (x) {
-          return x.userId == userId && x.friendId == friend.userId;
-        });
 
-        if (userExceptions.length === 0) {
-          this.secretFriend = friend.userName.toLocaleUpperCase();
+      var random = Math.floor(Math.random() * this.users.length) + 0;
+      var friend = this.users[random];
+
+      
+      var alreadyInUse = this.users.filter(function (x) {
+        return x.friendId == friend.userId;
+      });
+      //El amigo secreto no debe estar en uso
+      if (alreadyInUse.length === 0) {
+        //El amigo secreto debe ser diferente a el usuario seleccionado
+        if (friend.userId != userId) {
+          //El amigo secreto no debe estar como excepcion
+          var userExceptions = this.exceptions.filter(function (x) {
+            return x.userId == userId && x.friendId == friend.userId;
+          });
+
+          if (userExceptions.length === 0) {
+            this.secretFriend = friend.userName.toLocaleUpperCase();
+          }
         }
       }
+
     }
 
 
